cfc431175d3dc48af292f2bf5e363f4abe6af018
Nathan Lafferty <lcgamer333@gmail.com>
2020-06-14 16:37:53 -0500

    Add basic start/exit controls

---
+++ MenuController.gd
@@ -0,0 +1,24 @@
+extends MarginContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Start_Button_pressed():
+	var res = get_tree().change_scene("res://GameBoard.tscn")
+	if res != OK:
+		print("heavy failure")
+
+func _on_Exit_Button_pressed():
+	get_tree().quit()
